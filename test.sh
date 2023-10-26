560a04daa7ba7a61c1dfb648ab9f7da030355186
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-10-25 23:37:18 -0500

    [nc] add tflint
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
-echo 'start of test shell script'
-
-echo 'get terraform help'
-terraform --help
-
-echo 'try to install bats'
-sudo npm install -g bats@1.4.1
-
-echo 'all done'
+cat <<EOF > test.txt
+plugin "aws" {
+  enabled = true
+  source = "afkajhsdjkfh"
+}
+EOF
